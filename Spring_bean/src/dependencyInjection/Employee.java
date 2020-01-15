56479ca87c41f33c3d9ebd1edf57f5109778b856
khalid7487 <khalid15-7487@diu.edu.bd>
2020-01-15 14:36:39 +0600

    collection type inharitance

--- Spring_bean/src/dependencyInjection/Employee.java
+++ Spring_bean/src/dependencyInjection/Employee.java
@@ -9,28 +9,19 @@ import java.util.List;
  */
 public class Employee {
 
-    private Address address1;
-    private Address address2;
+    private List<Address> address1;
 
-    public Address getAddress1() {
+    public List<Address> getAddress1() {
         return address1;
     }
 
-    public void setAddress1(Address address1) {
+    public void setAddress1(List<Address> address1) {
         this.address1 = address1;
     }
-
-    public Address getAddress2() {
-        return address2;
-    }
-
-    public void setAddress2(Address address2) {
-        this.address2 = address2;
-    }
+    
     
     public void show() {
         System.out.println(address1);
-        System.out.println(address2);
     }
 
 }
