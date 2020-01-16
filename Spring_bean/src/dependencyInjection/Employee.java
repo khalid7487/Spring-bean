7e7c34a56ef7178d8a26a75c23cdd6c8724d64db
khalid7487 <khalid15-7487@diu.edu.bd>
2020-01-16 20:01:00 +0600

    required annotation

--- Spring_bean/src/dependencyInjection/Employee.java
+++ Spring_bean/src/dependencyInjection/Employee.java
@@ -2,6 +2,7 @@ package dependencyInjection;
 
 import java.util.Iterator;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Required;
 
 /**
  *
@@ -9,19 +10,18 @@ import java.util.List;
  */
 public class Employee {
 
-    private List<Address> address1;
+    private Address address1;
 
-    public List<Address> getAddress1() {
+    public Address getAddress1() {
         return address1;
     }
-
-    public void setAddress1(List<Address> address1) {
+    @Required
+    public void setAddress1(Address address1) {
         this.address1 = address1;
     }
-    
-    
+
     public void show() {
-        System.out.println(address1);
+        System.out.println(address1.toString());
     }
 
 }
